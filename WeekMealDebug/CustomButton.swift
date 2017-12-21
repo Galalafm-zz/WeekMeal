1d4a50af9e4ba312488d2a1f03471e54f9fa8f07
Galalafm <gala.pill@gmail.com>
2017-12-21 19:49:42 +0100

    fix scroll issue signup

--- WeekMealDebug/CustomButton.swift
+++ WeekMealDebug/CustomButton.swift
@@ -12,7 +12,6 @@ class CustomButton: UIButton {
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
         
-        self.layer.cornerRadius = 4.0
         self.layer.backgroundColor = UIColor(red:55/255.0, green:197/255.0, blue:93/255.0, alpha: 1.0).cgColor
         self.tintColor = UIColor.white
     }
